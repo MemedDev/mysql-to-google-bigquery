403464f2e8ebd092ab1642a2f3d741d7d4bc4114
james ransom <43888908+james-ransom@users.noreply.github.com>
2021-07-27 16:17:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ rm -fr vendor
 
 composer install 
 
+cp -v MySqlPlatform.php vendor/doctrine/dbal/lib/Doctrine/DBAL/Platforms/MySqlPlatform.php; 
+
 if [ -d "vendor" ] 
 then
     echo "Ok vendor directory is there continuing... " 
